610eb024689c473867acbd5253126e6bb180152e
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-08-12 11:56:35 +0300

    modified css file

--- mytodolist/src/App.js
+++ mytodolist/src/App.js
@@ -3,34 +3,13 @@ import React, { useState } from "react";
 import TodoList from "./Components/TodoList";
 
 function App() {
-  const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const handleAddTask = () => {
-    if (task) {
-      setTodos([...todos, task]);
-      setTask("");
-    }
-  };
-
-  const handleDeleteTask = (index) => {
-    const updatedTodos = todos.filter((_, i) => i !== index);
-    setTodos(updatedTodos);
-  };
+  
 
   
 
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        placeholder="Enter a new task"
-      />
-      <button onClick={handleAddTask}>Add Task</button>
-      <TodoList todos={todos} onDelete={handleDeleteTask} />
+<TodoList/>
     
     </div>
   );
--- mytodolist/src/Components/TodoList.js
+++ mytodolist/src/Components/TodoList.js
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import './TodoApp.css';
 
-function TodoList({ todos, onDelete }) {
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+
+  const addTask = () => {
+    if (taskText.trim() !== '') {
+      setTasks([...tasks, { text: taskText, done: false }]);
+      setTaskText('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const toggleDone = (index) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, done: !task.done } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (index, newText) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, text: newText } : task
+    );
+    setTasks(updatedTasks);
+  };
+
   return (
-    <ul>
-      {todos.map((todo, index) => (
-        <li key={index}>
-          {todo}
-          <button onClick={() => onDelete(index)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <div className="task-input">
+        <input
+          type="text"
+          placeholder="Add a task..."
+          value={taskText}
+          onChange={(e) => setTaskText(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index} className={task.done ? 'done' : ''}>
+            <input
+              type="checkbox"
+              checked={task.done}
+              onChange={() => toggleDone(index)}
+            />
+            <input
+              type="text"
+              value={task.text}
+              onChange={(e) => editTask(index, e.target.value)}
+            />
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default TodoList;

cb8980c1f4b2819fedd872b25ece5c91ce226cef
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-08-13 12:23:10 +0300

    modified index.css

--- mytodolist/src/Components/TodoList.js
+++ mytodolist/src/Components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import randomColor from "randomcolor";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
 
